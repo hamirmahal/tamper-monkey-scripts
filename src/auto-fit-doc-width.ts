7675fcdbb1eb35324ffa9f123b0318119c16eb6d
Hamir Mahal <hirehamir@gmail.com>
2025-03-30 14:17:58 -0700

    style: simplify `src/auto-fit-doc-width.ts`

--- src/auto-fit-doc-width.ts
+++ src/auto-fit-doc-width.ts
@@ -9,54 +9,29 @@
 // ==/UserScript==
 
 window.addEventListener("load", () => {
-	function attemptFit() {
-		const zoomSelectButton = document.getElementById("zoomSelect");
-		const mouseOptions = {
-			bubbles: true,
-			cancelable: true,
-			view: window,
-		};
-
-		if (!zoomSelectButton) {
-			console.warn("Failed to get Zoom select button. Retrying in 1 second...");
-			setTimeout(attemptFit, 1000);
-			return;
-		}
-
-		zoomSelectButton.dispatchEvent(new MouseEvent("mousedown", mouseOptions));
-
-		setTimeout(() => {
-			const allDivOptions = document.querySelectorAll('div[role="option"]');
-			const fitOption = Array.from(allDivOptions).find(
-				(option) => option.textContent?.trim() === "Fit",
-			);
-
-			if (fitOption) {
-				fitOption.dispatchEvent(new MouseEvent("mousedown", mouseOptions));
-				fitOption.dispatchEvent(new MouseEvent("mouseup", mouseOptions));
-				fitOption.dispatchEvent(new MouseEvent("click", mouseOptions));
-
-				console.log("Successfully selected 'Fit' from the zoom dropdown");
-			} else {
-				const allListOptions = document.querySelectorAll('li[role="option"]');
-				const listOption = Array.from(allListOptions).find(
-					(option) => option.textContent?.trim() === "Fit",
-				);
-
-				if (listOption) {
-					listOption.dispatchEvent(new MouseEvent("mousedown", mouseOptions));
-					listOption.dispatchEvent(new MouseEvent("mouseup", mouseOptions));
-					listOption.dispatchEvent(new MouseEvent("click", mouseOptions));
-					console.log("Successfully selected 'Fit' from zoom dropdown");
-				} else {
-					console.warn("Could not find the 'Fit' option after dropdown opened");
-					console.warn(
-						"You might need to inspect the dropdown's HTML structure further.",
-					);
-				}
-			}
-		}, 0);
-	}
-
-	attemptFit(); // Initial attempt after page load
+	attemptFit();
 });
+
+function attemptFit() {
+	const zoomSelectButton = document.getElementById("zoomSelect");
+	const mouseOptions = {
+		bubbles: true,
+		cancelable: true,
+		view: window,
+	};
+
+	zoomSelectButton?.dispatchEvent(new MouseEvent("mousedown", mouseOptions));
+
+	setTimeout(() => {
+		const allDivOptions = document.querySelectorAll('div[role="option"]');
+		const fitOption = Array.from(allDivOptions).find(
+			(option) => option.textContent?.trim() === "Fit",
+		);
+
+		fitOption?.dispatchEvent(new MouseEvent("mousedown", mouseOptions));
+		fitOption?.dispatchEvent(new MouseEvent("mouseup", mouseOptions));
+		fitOption?.dispatchEvent(new MouseEvent("click", mouseOptions));
+
+		console.log("Successfully selected 'Fit' from the zoom dropdown");
+	}, 0);
+}
